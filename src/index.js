b0ee4bd81e04939b34f3be0d9d71dd39cb5d9a2a
Rushikesh <rvp6123@gmail.com>
2025-09-22 08:46:20 +0530

    chore: add base Syncfusion packages, fix chart package name, downgrade some Syncfusion deps, update react-scripts, and add tailwindcss and html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDom from "react-dom";
-
-import './index.css'
+import ReactDOM from "react-dom";
+import './index.css';
 import App from './App';
 import { ContextProvider } from "./contexts/ContextProvider";
 
-ReactDom.render(
-<ContextProvider>
-    <App/>    
-</ContextProvider>,document.getElementById('root')
-); 
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
